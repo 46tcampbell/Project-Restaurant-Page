72d961b1f7a2eb78a2e066c01815c78322c674c3
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-11-12 13:08:00 -0600

    Update home page styling and content
    Decided to use a mix of an extra div and flexbox to make an easily
    centered card in the middle of the page.

--- src/home.js
+++ src/home.js
@@ -1,19 +1,26 @@
-import restaurantImage from './images/restaurant.jpg';
+import restaurantImage from './images/pirate.jpg';
+import './home.css';
 
 function loadHomepage() {
   const content = document.querySelector('#content');
   content.textContent = '';
+  const div = document.createElement('div');
+  div.classList.add('home-item');
+  const h1 = document.createElement('h1');
+  h1.textContent = "Captain Jim's Hideaway";
+  div.appendChild(h1);
   const img = document.createElement('img');
   img.src = restaurantImage;
-  img.alt = 'Interior view of an empty fancy looking restaurant';
-  content.appendChild(img);
-  const headline = document.createElement('h1');
-  headline.textContent = 'Elevate your palate with our culinary expertise';
-  content.appendChild(headline);
+  img.alt = 'Picture of Captain Jim summoning his next patrons';
+  div.appendChild(img);
+  const headline = document.createElement('h2');
+  headline.textContent = "Take your taste buds on a Seven Sea's Adventure";
+  div.appendChild(headline);
   const paragraph = document.createElement('p');
   paragraph.textContent =
-    "Come to Informal Jim's for all your laid-back needs. Whether you need to catch the game or meet up with friends after work, Informal Jim's has you covered. Grab a seat, get a drink and have a bite; good times included.";
-  content.appendChild(paragraph);
+    "Come to Captain Jim's Hideaway for all your pirate needs. Whether you need help catching the catch of the day or just need a little rum (we here it's always gone at other places), Captain Jim's Hideway has you covered. Grab a seat, get a drink and have a bite; ARRRGGGHHH.";
+  div.appendChild(paragraph);
+  content.appendChild(div);
 }
 
 export { loadHomepage };
